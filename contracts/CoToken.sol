6d7b17607e4fbd5b9952f3f84701c2de83cc8dff
Helda Mandlate <mndhel002@myuct.ac.za>
2019-08-05 00:37:47 +0200

    Done

--- contracts/CoToken.sol
+++ contracts/CoToken.sol
@@ -1,22 +1,31 @@
 pragma solidity^0.5.0;
 
+
+//import "github.com/OpenZeppelin/zeppelin-solidity/contracts/ownership/Ownable.sol";
+//import "github.com/OpenZeppelin/zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+//import "github.com/OpenZeppelin/zeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
+
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-//import "openzeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
 
 contract CoToken is Ownable, ERC20 {
 
-    uint256 _buyPrice;
-    uint256 _tokenSupply = 0;
-    uint256 _sellPrice;
+    uint256 public _buyPrice;
+    uint256 public _tokenSupply = 0;
+    uint256 public _sellPrice;
 
-    function buyPrice(uint256 _tokenSupply) public {
-        uint256 _buyPrice = (10**16)*(_tokenSupply) + (2*10**17); //Price is already in wei
+    function buyPrice(uint256 _buyTokens) public returns (uint){
+    //function buyPrice(uint256 _tokenSupply) public {
+        _buyPrice = ((5*(10**15))*(_buyTokens)*(_buyTokens)) + ((2*10**17)*(_buyTokens)); //Price is already in wei
+        //_buyPrice = ((10**16)*(_tokenSupplyi)) + (2*10**17);
+        //return(_buyPrice);
 
     }
 
-    function sellPrice(uint256 _tokenSupply) public {
-        uint256 _sellPrice = (10**16)*(_tokenSupply) + (2*10**17); //Price is already in wei
+    function sellPrice(uint256 _buyTokens) public {    
+    //function sellPrice(uint256 _tokenSupply) public {
+        _sellPrice = ((5*(10**15))*(_buyTokens)*(_buyTokens)) + ((2*10**17)*(_buyTokens)); //Price is already in wei
     }
 
     function mint(uint256 _n) public payable {
@@ -37,8 +46,5 @@ contract CoToken is Ownable, ERC20 {
     function destroy() public onlyOwner {
         selfdestruct(msg.sender);
     }
-
-
-
 
 }
